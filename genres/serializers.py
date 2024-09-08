d8a72369c600f2578a5758d228093e51f9c14299
almirmv <76816905+almirmv@users.noreply.github.com>
2024-09-07 22:46:02 -0300

    Aulas 124 a 128 convertendo para DRF
    Alterando de Function Based View do Dajango para Class Based Views do Django Rest Framework.

---
+++ genres/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from genres.models import Genre
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre       # qual modelo 
+        fields = '__all__'  # quais campos para serializar
