a36c22650656715c1bcc23c31da5bd0c4ab24977
almirmv <76816905+almirmv@users.noreply.github.com>
2024-09-10 21:51:31 -0300

    Aulas 141 a 147 Serializers
    Finalizado modulo serializers

--- movies/serializers.py
+++ movies/serializers.py
@@ -1,7 +1,29 @@
+from django.db.models import Avg
 from rest_framework import serializers
 from movies.models import Movie
 
-class MovieSerializer(serializers.ModelSerializer):
+class MovieModelSerializer(serializers.ModelSerializer):
+    # campo calculado
+    rate = serializers.SerializerMethodField(read_only=True)
+    
     class Meta:
         model = Movie
         fields = '__all__'
+
+    # metodo para o campo calculado. padaro "def_nomedocampocalculado(self, obj)
+    def get_rate(self, obj):
+        rate = obj.reviews.aggregate(Avg('stars'))
+        if rate:
+            return round(rate, 1)
+        return None        
+
+    # padrao de validate é "def validate_nomedacolunadodb(self,value)"
+    def validate_release_date(self, value):
+        if value.year < 1990:
+            raise serializers.ValidationError('A data de lançamento não pode ser anterior a 1990')
+        return value # nao deu erro simplesmente retorna o value
+    
+    def validate_resume(self, value):
+        if len(value) > 200:
+            raise serializers.ValidationError('Resumo não pode ser maior que 200 caracteres')
+        return value   
