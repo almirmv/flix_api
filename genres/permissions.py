a1163353c5e07e097a4f8d1a8a9f90b43f832dfd
almirmv <76816905+almirmv@users.noreply.github.com>
2024-09-14 17:56:45 -0300

    correção aula 158

--- genres/permissions.py
+++ genres/permissions.py
@@ -4,7 +4,14 @@ from rest_framework import permissions
 class GenrePermissionClass(permissions.BasePermissions):
     def has_permission(self, request,view):
         # podemos validar o que quiser aqui
-        if request.method in ['GET', 'OPTIONS', 'HEAD']:
-            # view, add, change, delete ('nomedoapp.permissao_model')
+        # view, add, change, delete ('nomedoapp.permissao_model')
+        if request.method in ['GET', 'OPTIONS', 'HEAD']:            
             return request.user.has_permission('genres.view_genre')
+        if request.method == 'POST':
+            return request.user.has_permission('genres.add_genre')
+        if request.method in ['PUT', 'PATH']:
+            return request.user.has_permission('genres.change_genre')
+        if request.method == 'DELETE':
+            return request.user.has_permission('genres.delete_genre')
+        
         return False
